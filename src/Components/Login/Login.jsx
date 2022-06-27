d696142d2316c5c5b84bc8c5952601b5a5d0c90f
vivek <vivekkumarvky13@gmail.com>
2022-06-28 00:34:02 +0530

    Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,13 +3,28 @@ import "./Login.css"
 export const Login = () =>{
     return (
       <div>
-        {/* { <img
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
-          alt=""
-        /> } */}
-        <div id="secondbox"></div>
+        <div id="secondbox">
+          
+          <h1>Sign In</h1>
+           <label htmlFor="">E-mail</label>
+          <input type="text" placeholder="Email"/>
+          <br />
+          <label htmlFor="">Password</label>
+          <input type="password" placeholder="Password" />
+          <br />
+          <button>Sign In</button>
+        </div>
 
-        <div id="firstbox"></div>
+        <div id="firstbox">
+          
+            <img
+              id="imgtitle"
+              src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+              alt=""
+            />
+            <b id="title">WHATSAPP WEB</b>
+          
+        </div>
 
         <div id="thirdbox"></div>
       </div>
