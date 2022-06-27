803512efd12bf8c687e0265d03adb8066f18da77
vivek <vivekkumarvky13@gmail.com>
2022-06-27 21:55:39 +0530

    Login

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import "./Login.css"
+
+export const Login = () =>{
+    return (
+      <div>
+        {/* { <img
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+          alt=""
+        /> } */}
+        <div id="secondbox"></div>
+
+        <div id="firstbox"></div>
+
+        <div id="thirdbox"></div>
+      </div>
+    );
+}
