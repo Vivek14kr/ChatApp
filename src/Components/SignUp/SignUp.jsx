f682902d0679bd39a562a7e6c271bce1a7843642
vivek <vivekkumarvky13@gmail.com>
2022-06-28 23:21:30 +0530

    signup

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,7 +1,9 @@
-import {Card, Button, Form} from "react-bootstrap"
-import {useRef} from "react";
+import {Card, Button, Form, Alert} from "react-bootstrap"
+import {useRef, useState} from "react";
 import {Container} from "react-bootstrap"
 import {useAuth} from "../../contexts/AuthContext"
+import "./SignUp.css"
+
 export const SignUp = ()=>{
 
 
@@ -9,20 +11,45 @@ export const SignUp = ()=>{
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
   const {signup} = useAuth()
-
-  function handleSubmit(e){
+ const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+  async function handleSubmit(e){
     e.preventDefault()
-    signup(emailRef.current.value, passwordRef.current.value)
+console.log(
+  passwordRef,
+  emailRef,
+  " new way ",
+  passwordRef.current.value === passwordConfirmRef.current.value
+);
+    if (passwordRef.current.value !==
+       passwordConfirmRef.current.value){
+      return setError("Passwords do not match")
+    }
+
+    try{ 
+      setError("")
+      setLoading(true)
+      await
+signup(emailRef.current.value, 
+      passwordRef.current.value)
+    }
+    catch{
+      setError("Failed to create an account")
+    }
+    setLoading(false)
+    
   }
    return (
      <>
-     <Container className="d-flex align-items-center justify-content-center" 
+     <div id="secondbox" className="d-flex align-items-center justify-content-center newcolor" 
      style={{minHeight:"100vh"}}>
-      <div className="w-100" style={{maxWidth:"400px"}}>
+      <div id="cardd" className="w-100" style={{maxWidth:"400px"}}>
        <Card>
          <Card.Body>
            <h2 className="text-center mb-4">Sign Up</h2>
-           <Form>
+           {error && <Alert variant="danger">{error}
+           </Alert>}
+           <Form onSubmit={handleSubmit}>
              <Form.Group id="email">
                <Form.Label>Email</Form.Label>
                <Form.Control type="email" ref={emailRef} required />
@@ -35,17 +62,29 @@ export const SignUp = ()=>{
                <Form.Label>Password Confirmation</Form.Label>
                <Form.Control type="password" ref={passwordConfirmRef} required />
              </Form.Group>
-             <Button className="w-100" type="submit">
+             <Button disabled={loading} className="w-100" type="submit">
                Sign Up
              </Button>
            </Form>
          </Card.Body>
        </Card>
        <div className="w-100 text-center mt-2">
-         Already have an account? Login In
+         Already have an account? Login 
        </div>
        </div>
-       </Container>
+       </div>
+        <div id="firstbox">
+          
+            <img
+              id="imgtitle"
+              src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+              alt=""
+            />
+            <b id="title">WHATSAPP WEB</b>
+          
+        </div>
+
+        <div id="thirdbox"></div>
      </>
    );
 }
